a695f8f2db7c5a8591112c4bc5a6b982306d37ec
binio <tbiniecki@gmail.com>
2017-09-11 12:53:59 +0100

    add model

---
+++ app/controllers/Products.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by tomasz.biniecki on 11/09/2017.
+ */
+public class Products extends Controller {
+
+    public Result list() {
+        return TODO;
+    }
+
+    public Result newProduct() {
+        return TODO;
+    }
+
+    public Result details(String ean) {
+        return TODO;
+    }
+
+    public Result save() {
+        return TODO;
+    }
+}
